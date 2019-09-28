5caaa19bed754c0a935a2adbc6bc54d814976ffa
karim12345 <k.abdelmegeed@nouracademy.com>
2019-09-29 01:06:55 +0200

    separate async request

--- lib/carosel.dart
+++ lib/carosel.dart
@@ -1,70 +1,24 @@
-import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
 import 'images.dart';
-import 'dart:async';
 
+List<String> imgList2 = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
+void main() => runApp(CarouselDemo());
 
 final Widget placeholder = Container(color: Colors.grey);
 
-List<String> imagesList;
-var ret = new List();
- Future<List>images() async{
- imagesList=await callEndPoint();
-
- imagesList.forEach(
-         (v) =>
-             ret.add(v)
-
-  );
-  return ret.toList();
-
-}
-final List child = map<Widget>(
-  ret,
-      (index, i) {
-    return Container(
-      margin: EdgeInsets.all(5.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.all(Radius.circular(5.0)),
-        child: Stack(children: <Widget>[
-          Image.network(i, fit: BoxFit.cover, width: 1000.0),
-          Positioned(
-            bottom: 0.0,
-            left: 0.0,
-            right: 0.0,
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color.fromARGB(200, 0, 0, 0),
-                    Color.fromARGB(0, 0, 0, 0)
-                  ],
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                ),
-              ),
-              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              child: Text(
-                'No. $index image',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-        ]),
-      ),
-    );
-  },
-).toList();
 List<T> map<T>(List list, Function handler) {
   List<T> result = [];
   for (var i = 0; i < list.length; i++) {
     result.add(handler(i, list[i]));
   }
-
   return result;
 }
 
@@ -75,6 +29,55 @@ class CarouselWithIndicator extends StatefulWidget {
 
 class _CarouselWithIndicatorState extends State<CarouselWithIndicator> {
   int _current = 0;
+  List child;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Images imgList = Images();
+    print(imgList.hotelImages);
+    child = map<Widget>(
+      imgList.hotelImages,
+      (index, i) {
+        return Container(
+          margin: EdgeInsets.all(5.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(5.0)),
+            child: Stack(children: <Widget>[
+              Image.network(i, fit: BoxFit.cover, width: 1000.0),
+              Positioned(
+                bottom: 0.0,
+                left: 0.0,
+                right: 0.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(200, 0, 0, 0),
+                        Color.fromARGB(0, 0, 0, 0)
+                      ],
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                    ),
+                  ),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  child: Text(
+                    'No. $index image',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+          ),
+        );
+      },
+    ).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -93,8 +96,8 @@ class _CarouselWithIndicatorState extends State<CarouselWithIndicator> {
       Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: map<Widget>(
-          imagesList,
-              (index, url) {
+          imgList2,
+          (index, url) {
             return Container(
               width: 8.0,
               height: 8.0,
@@ -115,14 +118,6 @@ class _CarouselWithIndicatorState extends State<CarouselWithIndicator> {
 class CarouselDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //Manually operated Carousel
-    final CarouselSlider manualCarouselDemo = CarouselSlider(
-      items: child,
-      autoPlay: false,
-      enlargeCenterPage: true,
-      viewportFraction: 0.9,
-      aspectRatio: 2.0,
-    );
     return MaterialApp(
       title: 'demo',
       home: Scaffold(
@@ -130,14 +125,11 @@ class CarouselDemo extends StatelessWidget {
         body: ListView(
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.symmetric(vertical: 15.0),
-              child: Column(
-                children: [
-                  Text('Manuell Carousel'),
-                  manualCarouselDemo,
-                ],
-              ),
-            ),
+                padding: EdgeInsets.symmetric(vertical: 15.0),
+                child: Column(children: [
+                  Text('Carousel With Indecator'),
+                  CarouselWithIndicator(),
+                ])),
           ],
         ),
       ),
--- lib/images.dart
+++ lib/images.dart
@@ -2,18 +2,18 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
-
-
- Future<List> callEndPoint() async {
-  var url =
-      "http://api.syal.travninja.syal.com.sa/hotel_details?hotel_code=1128571";
-  var response = await http.get(url);
-  var obj = convert.jsonDecode(response.body);
-  var hotelObj= obj['hotel']['images'];
-  List hotelImages = [];
-  for( var i=0; i < hotelObj.length ;i++){
-    hotelImages.add(hotelObj[i]['path']);
+class Images {
+  List<String> hotelImages;
+  void callEndPoint() async {
+    var url =
+        "http://api.syal.travninja.syal.com.sa/hotel_details?hotel_code=1128571";
+    var response = await http.get(url);
+    var obj = convert.jsonDecode(response.body);
+    var hotelObj = obj['hotel']['images'];
+    List apiImages = [];
+    for (var i = 0; i < hotelObj.length; i++) {
+      apiImages.add(hotelObj[i]['path']);
+    }
+    hotelImages = apiImages;
   }
-  return hotelImages;
 }
-
